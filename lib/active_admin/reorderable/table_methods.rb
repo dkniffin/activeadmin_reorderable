1d4363c7ec882346a18711f757d41933f8eaa360
Chris Jones <chris.jones@viget.com>
2014-11-05 23:52:09 -0500

    Fix error when used in namespaces other than admin

--- lib/active_admin/reorderable/table_methods.rb
+++ lib/active_admin/reorderable/table_methods.rb
@@ -11,7 +11,7 @@ module ActiveAdmin
       private
 
       def reorder_handle_for(resource)
-        url = url_for([:reorder, :admin, resource])
+        url = url_for([:reorder, active_admin_namespace.name, resource])
         span(reorder_handle_content, :class => 'reorder-handle', 'data-reorder-url' => url)
       end
 
